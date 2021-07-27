01fdf2067d15dc4645f2f548c79119111f5320c3
BryceKrispy9 <brycepearson09@gmail.com>
2021-07-26 20:21:49 -0600

    commit on master

--- Python Learning/Flask_app/app.py
+++ Python Learning/Flask_app/app.py
@@ -56,7 +56,22 @@ def get_guides():
 @app.route("/guide/<id>", methods=["GET"])
 def get_guide(id):
     guide = Guide.query.get(id)
-    return guides_schema.jsonify(guide)
+    return guide_schema.jsonify(guide)
+
+
+# Endpoint for updating a guide
+@app.route("/guide/<id>", methods=["PUT"])
+def guide_update(id):
+    guide = Guide.query.get(id)
+    title = request.json['title']
+    content = request.json['content']
+
+    guide.title = title
+    guide.content = content
+
+    db.session.commit()
+    return guide_schema.jsonify(guide)
+
 
 
 if __name__ == '__main__':
