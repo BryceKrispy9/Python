ed5ff887c4bfaf9505b27062a48d0dfc1b3cafb7
BryceKrispy9 <brycepearson09@gmail.com>
2021-07-26 21:21:31 -0600

    commit on master

--- Python Learning/mongo.js
+++ Python Learning/mongo.js
@@ -1,10 +1,18 @@
-db.createUser({
-    user: 'Devyn',
-    pwd: 'password',
-    customData: { startDate: new Date() },
-    roles: [
-      { role: 'clusterAdmin', db: 'admin' },
-      { role: 'readAnyDatabase', db: 'admin' },
-      'readWrite'
-    ]
-  })
+// db.createUser({
+//     user: 'Devyn',
+//     pwd: 'password',
+//     customData: { startDate: new Date() },
+//     roles: [
+//       { role: 'clusterAdmin', db: 'admin' },
+//       { role: 'readAnyDatabase', db: 'admin' },
+//       'readWrite'
+//     ]
+//   })
+
+// db.books.insert({
+//     "title": "OOP Programming",
+//     "startDate": new Date(),
+//     "authors": [
+//         { "name": "Jon Snow Jr" }
+//     ]
+// })
